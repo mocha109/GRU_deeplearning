7b0fb589dc7e412a312a2da10378a1d72bacf06c
jerry22867 <gle22867@gmail.com>
2021-06-02 22:26:24 +0800

    加入train_gru

---
+++ train_gru.py
@@ -0,0 +1,21 @@
+import sys
+sys.path.append('..')
+from common.optimizer import SGD
+from common.trainer import GRUTrainer
+from common.util import eval_perplexity
+
+from model import Rnngru
+
+
+#設定超參數
+batch_size = 
+var_size = 
+output_size = 
+hidden_size = 
+time_size = 
+lr = 
+max_epoch = 
+max_grad = 
+
+
+#載入學習資料
