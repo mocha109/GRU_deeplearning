057cdebb764456b230fe10099d776ea7360c4335
jerry22867 <gle22867@gmail.com>
2021-06-02 16:45:51 +0800

    1

--- model.py
+++ model.py
@@ -17,5 +17,31 @@ class Rnngru:
 
         #產生各層
         self.layers = [
-            TimeGRU()
+            TimeGRU(gru_Wx,gru_Wh,gru_b,stateful=True),
+            TimeConnection(),
+            TimeAffine(affine_w,affine_b)
         ]
+        self.loss_layer = TimeSoftmaxWithLoss()
+        self.lstm_layer = self.layers[1]
+        #把所有權重與梯度整合成清單
+        self.params, self.grads = [], []
+        for layer in self.layers:
+            self.params += layer.params
+            self.grads += layer.grads
+
+    def predict(self, xs):
+        for layer in self.layers:
+            xs = layer.forward(xs)
+        return xs
+
+    #還在看
+    def forward(self, xs, ts):
+        score = self.predict(xs)
+        loss = self.loss_layer.forward(score, ts)
+        return loss 
+
+    def backward(self, dout=1):
+        dout = self.loss_layer.backward(dout)
+        for layer in reversed(self.layers):
+            dout = layer.backward(dout)
+        return dout
